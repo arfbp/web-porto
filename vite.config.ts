4b28deab4fbf7caf083b2798e33bfc9966fda686
arfbp <79461036+arfbp@users.noreply.github.com>
2026-05-22 16:20:42 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/web-porto/',
+    
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
